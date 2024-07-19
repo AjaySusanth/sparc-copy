03789751d597a7781bd306e325493e4983ce83f7
Joann <joannjkoodathil@gmail.com>
2024-07-20 00:39:13 +0530

    memories made responsive

--- src/Pages/Memories/Memories.jsx
+++ src/Pages/Memories/Memories.jsx
@@ -3,20 +3,21 @@ import './Memories.css';
 import memories from "../../assets/Images/memories.png";
 import icon from "../../assets/Images/corner icon.png";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
+import WhiteBg from '../WhiteBg/WhiteBg';
 function Memories() {
   return (
     <>
+    
      <section className='memories'>
      <img src={icon} alt="memories" className='cornericon' />
       
       <h1> memories</h1>
       
       <img src={memories} alt="memories" className='memoryimg' />
-       <button>  <i class="bi bi-play-circle-fill"> </i>     Know More .   </button>
+       <button>  <i class="bi bi-play-circle-fill"> </i>     Know More    </button>
      </section>
       
-
+     
     </>
   )
 }
