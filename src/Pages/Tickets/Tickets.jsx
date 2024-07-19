9e1e02656c225b68275595f1875d518eb47fe6b0
Aharon Johnson <ahuisash@gmail.com>
2024-07-20 05:24:49 +0530

    Sponsor section, Ticket Section

--- src/Pages/Tickets/Tickets.jsx
+++ src/Pages/Tickets/Tickets.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import './Tickets.css'
+import icon from "../../assets/Images/corner icon.png";
+import t1 from "../../assets/Images/blue ticket.png";
+import t2 from "../../assets/Images/white ticket.png";
 
 function Tickets() {
   return (
-    <div>
+    <section id='ticket' className='sectionstyle'>
+      <div className="content">
+        <img src={icon} className='rightcornericon'/>
+        <h1>GRAB YOUR TICKETS</h1>
+        <div className="cards">
+          <div className="ticket t1">
+            <img src={t1} className='tkt-icon' alt='tkt-icon'></img>
+            <h2>IEEE MEMBERS</h2>
+            <button className='reg-btn'>Register Now ↗</button>
+          </div>
+          <div className="ticket t2">
+            <img src={t2} alt='tkt-icon' className='tkt-icon'></img>
+            <h2>NON IEEE MEMBERS</h2>
+            <button className='reg-btn'>Register Now ↗</button>
+          </div>
+        </div>
+      </div>
       
-    </div>
+    </section>
   )
 }
 
