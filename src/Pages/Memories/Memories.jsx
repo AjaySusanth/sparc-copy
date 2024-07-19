e5891e996f3924704d34ab105517b8dbd7ae4552
Joann <joannjkoodathil@gmail.com>
2024-07-19 21:58:22 +0530

    Memories section added

--- src/Pages/Memories/Memories.jsx
+++ src/Pages/Memories/Memories.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import './Memories.css'
+import React from 'react';
+import './Memories.css';
+import memories from "../../assets/Images/memories.png";
+import icon from "../../assets/Images/corner icon.png";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function Memories() {
   return (
-    <div>
+    <>
+     <section className='memories'>
+     <img src={icon} alt="memories" className='cornericon' />
       
-    </div>
+      <h1> memories</h1>
+      
+      <img src={memories} alt="memories" className='memoryimg' />
+       <button>  <i class="bi bi-play-circle-fill"> </i>     Know More   </button>
+     </section>
+      
+
+    </>
   )
 }
 
