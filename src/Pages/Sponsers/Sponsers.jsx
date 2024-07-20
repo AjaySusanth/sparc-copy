1562b2f05c4f9ada0f4f5cb441a2b6f8d8ea0afb
Aharon Johnson <ahuisash@gmail.com>
2024-07-20 20:30:54 +0530

    Minor corrections in Tickets,Sponsorship

--- src/Pages/Sponsers/Sponsers.jsx
+++ src/Pages/Sponsers/Sponsers.jsx
@@ -5,6 +5,7 @@ import ieeestdlogo from "../../assets/Images/ieee students.png";
 import ieeereglogo from "../../assets/Images/ieee region 10.png";
 import ieeelogo from "../../assets/Images/ieee logo white.png";
 import Heading from '../Heading/Heading';
+import CornerIcon from "../RCornericon/RCornericon";
 
 
 
@@ -12,6 +13,7 @@ function Sponsers() {
   return (
     <section id='sponsers'>
       <div className="content sponser-cont">
+        <CornerIcon/>
         <Heading text= "SPONSORS" />
         <div className="sp-card">
           <img src={macesblogo} alt="ieee-logo"/>
